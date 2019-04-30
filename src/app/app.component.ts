f95ee77cba19d7f7da121adc89731c3716497e4a
ErikNijland <e.n.nijland@gmail.com>
2019-04-30 11:22:34 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalService } from './modal/modal.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   constructor(private modalService: ModalService) {}
+
+  ngOnInit() {
+    setTimeout(() => this.showModal());
+  }
 
   showModal() {
     this.modalService.openModal('myModal');
