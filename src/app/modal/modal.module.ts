013ca8687e536738e921f7c1a691677131e47772
ErikNijland <e.n.nijland@gmail.com>
2019-04-30 12:04:28 +0200

    5 files modified

--- src/app/modal/modal.module.ts
+++ src/app/modal/modal.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {A11yModule} from '@angular/cdk/a11y';
 
 import {ModalComponent} from './modal.component';
 import {ModalService} from './modal.service';
@@ -12,6 +13,7 @@ import {ModalService} from './modal.service';
     ModalService,
   ],
   imports: [
+    A11yModule,
     CommonModule,
   ],
   exports: [
