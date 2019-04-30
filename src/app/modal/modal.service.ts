c940031386c70c21534867294346d8ae793b7c96
ErikNijland <e.n.nijland@gmail.com>
2019-04-30 11:00:46 +0200

    Make modalservice

--- src/app/modal/modal.service.ts
+++ src/app/modal/modal.service.ts
@@ -1,4 +1,23 @@
 import {Injectable} from '@angular/core';
+import {BehaviourSubject, Observable} from 'rxjs';
 
 @Injectable()
-export class ModalService {}
+export class ModalService {
+  modals: { [modalName: string]: BehaviourSubject<boolean> } = {};
+
+  registerModal(modalName: string) {
+    this.modals[modalName] = new BehaviourSubject(false);
+  }
+
+  openModal(modalName: string) {
+    this.modals[modalName].next(true);
+  }
+
+  closeModal(modalName: string) {
+    this.modals[modalName].next(false);
+  }
+
+  isOpen(modalName: string): Observable<boolean> {
+    return this.modals[modalName];
+  }
+}
